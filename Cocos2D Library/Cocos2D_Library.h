9d8eb7239396ede2a28af3d2de747720fdb18ff1
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-07-04 15:50:42 -0400

    master: updated project to be ARC compliant

---
+++ Cocos2D Library/Cocos2D_Library.h
@@ -0,0 +1,13 @@
+//
+//  Cocos2D_Library.h
+//  Cocos2D Library
+//
+//  Created by Hamdan Javeed on 2013-07-04.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cocos2D_Library : NSObject
+
+@end
---
+++ Cocos2D Library/Cocos2D_Library.m
@@ -0,0 +1,13 @@
+//
+//  Cocos2D_Library.m
+//  Cocos2D Library
+//
+//  Created by Hamdan Javeed on 2013-07-04.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import "Cocos2D_Library.h"
+
+@implementation Cocos2D_Library
+
+@end
