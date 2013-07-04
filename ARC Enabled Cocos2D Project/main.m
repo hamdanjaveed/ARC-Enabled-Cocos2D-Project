5194f843edb6b299a17b89463e55ad17c4774caf
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-07-04 15:44:35 -0400

    Initial Commit

---
+++ ARC Enabled Cocos2D Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ARC Enabled Cocos2D Project
+//
+//  Created by Hamdan Javeed on 2013-07-04.
+//  Copyright Hamdan Javeed 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
